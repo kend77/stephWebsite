fddec3b896b0b551e36da0665b1455300f53fb42
Kenny Diaz <kend77@gmail.com>
2017-12-19 13:59:06 -0500

    add navbar

---
+++ src/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './navbar.css'
+const NavBar = () => {
+	return (
+	<div id="navbar">
+	<h1>Stephanie Diaz</h1>
+	<ul id="nav-options">
+		<li>schumacher</li>
+		<li>architectural digest</li>
+		<li>self</li>
+		<li>downtown</li>
+		<li>about</li>
+	</ul>
+	</div>
+	)
+	}
+
+export default NavBar;
