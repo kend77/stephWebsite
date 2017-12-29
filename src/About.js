7cd7ca8a0319a7f3a7053b433339fc8d4ebc03f9
Kenny Diaz <kend77@gmail.com>
2017-12-29 13:14:34 -0500

    add eslint config, about section centered

--- src/About.js
+++ src/About.js
@@ -1,36 +1,27 @@
 import React from 'react'
-import { Image, Divider, Container, Header, List, Icon } from 'semantic-ui-react'
+import { Image, List, Icon } from 'semantic-ui-react'
 import './about.css'
-import { Link } from 'react-router-dom'
-
-
-
 
 const About = () => {
   return (
-    <div id="about" style={{display: 'flex'}}>
-      <Image src="/images/steph.jpg" />
-      <Container>
+    <div id="about">
+      <div id="potrait-container">
+      <Image id="potrait" src="/images/stephanie.jpg" />
+      </div>
+      <div id="bio-container">
         <h2>About Me</h2>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
+        <p id="bio">Stephanie, a NYC graphic designer, attended Penn State University. During her first year she discovered her love for design and typography after learning names like Chip Kidd, Alexey Brodovich, and Saul Bass. She has previously worked for SELF magazine and Architectural Digest. It was at Architectural Digest where she developed an interest for interior design. Currently she is the Senior Designer for Schumacher and Patterson Flynn Martin where she has been able to combine her love for all things creative.</p>
         <List bulleted horizontal link>
-          <List.Item as='a'>
-            <a href="mailto:stephanie.diaz26@gmail.com">
-            <Icon name='mail'/>
+          <List.Item as="a" href="mailto:stephanie.diaz26@gmail.com" >
+            <Icon name="mail" />
             Email Me
-            </a>
           </List.Item>
-          <List.Item as='a'>
-            <Link to="https://www.linkedin.com/in/stephanie-diaz-382ab561/" target='_blank'>
-              <Icon name='linkedin'>
-              </Icon>
+          <List.Item as="a" href="https://www.linkedin.com/in/stephanie-diaz-382ab561/" target="_blank">
+              <Icon name="linkedin" />
               LinkedIn
-            </Link>
           </List.Item>
         </List>
-      </Container>
+      </div>
     </div>
   )
 }
